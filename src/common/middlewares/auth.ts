714b954e9d8d7663b251c3580f84f3217547af6b
Asary <asary.info@gmail.com>
2021-10-02 16:45:51 +0700

    🔥 Removed unused files
    Signed-off-by: Asary <asary.info@gmail.com>

--- src/common/middlewares/auth.ts
+++ src/common/middlewares/auth.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express";
-import UserRepository from "modules/users/repository";
+// import UserRepository from "modules/users/repository";
 import * as jwt from "jsonwebtoken";
 import config from "../../config/config";
 
 import { fail, success } from "common/responses";
-import { user } from "modules/users/mapper";
+// import { user } from "modules/users/mapper";
 // import * as _ from "underscore";
 
 /************************
@@ -13,29 +13,30 @@ import { user } from "modules/users/mapper";
 const checkJwt = async (req: Request, res: Response, next: NextFunction) => {
   //Get the jwt token from the head
 
-  const token = <string>req.headers.authorization;
-  let jwtPayload;
-  //Try to validate the token and get data
-  try {
-    // Validate the JWT
-    jwtPayload = <any>jwt.verify(token, config.server.jwt_secret);
-    res.locals.jwtPayload = jwtPayload;
+  return next();
+  // const token = <string>req.headers.authorization;
+  // let jwtPayload;
+  // //Try to validate the token and get data
+  // try {
+  //   // Validate the JWT
+  //   jwtPayload = <any>jwt.verify(token, config.server.jwt_secret);
+  //   res.locals.jwtPayload = jwtPayload;
 
-    // Check if User Exists
-    await UserRepository.getOneByID(jwtPayload.id)
-      .then((userData: any) => {
-        // return user with Roles and Perms to the next middleware
-        res.locals.user = user(userData);
-        return next();
-      })
-      .catch((err: any) => {
-        // return 401 if user does not exist
-        return res.status(401).send(fail(new Error(err), "user not Found!"));
-      });
-  } catch (error: any) {
-    //If token is not valid, respond with 401 (unauthorized)
-    return res.status(401).send(fail(error));
-  }
+  //   // Check if User Exists
+  //   await UserRepository.getOneByID(jwtPayload.id)
+  //     .then((userData: any) => {
+  //       // return user with Roles and Perms to the next middleware
+  //       res.locals.user = user(userData);
+  //       return next();
+  //     })
+  //     .catch((err: any) => {
+  //       // return 401 if user does not exist
+  //       return res.status(401).send(fail(new Error(err), "user not Found!"));
+  //     });
+  // } catch (error: any) {
+  //   //If token is not valid, respond with 401 (unauthorized)
+  //   return res.status(401).send(fail(error));
+  // }
 };
 
 export default checkJwt;
--- src/modules/TournamentResult/routes.ts
+++ src/modules/TournamentResult/routes.ts
@@ -3,12 +3,9 @@ const router = Router();
 
 import getAll from "./useCases/getAll";
 import getOne from "./useCases/getOneByID";
-import destroy from "./useCases/destroy";
 import create from "./useCases/create";
-import update from "./useCases/update";
 
 import createValidation from "./validation/create";
-import updateValidation from "./validation/update";
 
 import validate from "common/middlewares/validationMiddleware";
 
